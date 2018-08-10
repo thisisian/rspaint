f63c4d178d634e81324a61201a35c1b4ebe7160a
Ian Winter <iwinter@pdx.edu>
2018-08-09 21:03:52 -0700

    Now you can select pencil or eraser tools

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,24 @@ use std::option::Option::*;
 pub mod enums;
 use enums::*;
 
+// Make cloning simpler for closures (from http://gtk-rs.org/tuto/closures)
+macro_rules! clone {
+    (@param _) => ( _ );
+    (@param $x:ident) => ( $x );
+    ($($n:ident),+ => move || $body:expr) => (
+        {
+            $( let $n = $n.clone(); )+
+            move || $body
+        }
+    );
+    ($($n:ident),+ => move |$($p:tt),+| $body:expr) => (
+        {
+            $( let $n = $n.clone(); )+
+            move |$(clone!(@param $p),)+| $body
+        }
+    );
+}
+
 fn build_ui(application: &gtk::Application) {
     let window = ApplicationWindow::new(application);
 
@@ -41,7 +59,7 @@ fn build_ui(application: &gtk::Application) {
     h_box.pack_start(&tool_box, false, false, 0);
 
     let canvas = gtk::DrawingArea::new();
-    configure_canvas(&canvas);
+    configure_canvas(&canvas, tool_state.clone());
 
     v_box.pack_start(&canvas, false, false, 10);
     h_box.pack_start(&v_box, false, false, 10);
@@ -51,7 +69,7 @@ fn build_ui(application: &gtk::Application) {
     window.show_all();
 }
 
-fn configure_canvas(canvas: &gtk::DrawingArea) {
+fn configure_canvas(canvas: &gtk::DrawingArea, tool_state: Rc<RefCell<Option<Tool>>>) {
     canvas.set_size_request(400, 400);
     let surface: Rc<RefCell<Option<cairo::Surface>>> = Rc::new(RefCell::new(None));
     let clear_surface = |surf: &cairo::Surface| {
@@ -87,6 +105,7 @@ fn configure_canvas(canvas: &gtk::DrawingArea) {
     // When mouse is clicked on canvas
     let surface_clone = surface.clone();
     let last_position_clone = last_position.clone();
+    let tool_state_clone = tool_state.clone();
     canvas.connect_button_press_event(move |canv, event| {
         let (x, y) = event.get_position();
         draw_square(canv, surface_clone.borrow().as_ref().unwrap(), x, y);
@@ -94,31 +113,41 @@ fn configure_canvas(canvas: &gtk::DrawingArea) {
         Inhibit(false)
     });
 
+    // TODO: Reset last position when button is released
+
     // When cursor moves across canvas
     let surface_clone = surface.clone();
     let last_position_clone = last_position.clone();
+    let tool_state_clone = tool_state.clone();
     canvas.connect_motion_notify_event(move |canv, event| {
         let (x, y) = event.get_position();
         let state = event.get_state();
+        let cur_tool = tool_state_clone.borrow().clone();
         let last_position_exists = last_position_clone.borrow().as_ref().is_some();
         if state == gdk::ModifierType::BUTTON1_MASK {
-            if last_position_exists == true {
-                let last_x  = last_position_clone.borrow().as_ref().unwrap().0;
-                let last_y  = last_position_clone.borrow().as_ref().unwrap().1;
-                let cr = cairo::Context::new(&surface_clone.borrow().as_ref().unwrap());
-                cr.set_antialias(cairo::Antialias::None);
-                cr.move_to(last_x, last_y);
-                cr.line_to(x, y);
-                cr.set_line_width(1.0);
-                cr.stroke();
-                canv.queue_draw(); // TODO: More efficient??
-            } else {
-                draw_square(canv, surface_clone.borrow().as_ref().unwrap(), x, y);
-                last_position_clone.replace(Some((x,y)));
+            match cur_tool {
+                Some(Tool::Pencil) => {
+                    if last_position_exists == true {
+                        let last_x = last_position_clone.borrow().as_ref().unwrap().0;
+                        let last_y = last_position_clone.borrow().as_ref().unwrap().1;
+                        let cr = cairo::Context::new(&surface_clone.borrow().as_ref().unwrap());
+                        cr.set_antialias(cairo::Antialias::None);
+                        cr.move_to(last_x, last_y);
+                        cr.line_to(x, y);
+                        cr.set_line_width(1.0);
+                        cr.stroke();
+                        canv.queue_draw(); // TODO: How to make more efficient??
+                    } else {
+                        draw_square(canv, surface_clone.borrow().as_ref().unwrap(), x, y);
+                        last_position_clone.replace(Some((x, y)));
+                    }
+                }
+                Some(Tool::Eraser) => {},
+                _ => {},
             }
         }
-        last_position.replace(Some((x, y)));
-        Inhibit(false)
+    last_position.replace(Some((x, y)));
+    Inhibit(false)
     });
 
     // Register the events so that they will work.
@@ -146,6 +175,25 @@ fn build_tool_box(tool_box: &gtk::Box,
     let eraser_icon = gtk::Image::new_from_icon_name("face-sad", gtk::IconSize::SmallToolbar.into());
     pencil_button.set_image(&pencil_icon);
     eraser_button.set_image(&eraser_icon);
+    pencil_button.set_label("Pencil");
+    eraser_button.set_label("Eraser");
+
+    let eraser_button_clone = eraser_button.clone();
+    let pencil_button_clone = pencil_button.clone();
+    let tool_state_clone = tool_state.clone();
+    eraser_button.connect_toggled(move |this| {
+        if this.get_active() == true {
+            tool_state_clone.replace(Some(Tool::Eraser));
+            pencil_button_clone.set_active(false);
+        }
+    });
+    let tool_state_clone = tool_state.clone();
+    pencil_button.connect_toggled(move |this| {
+        if this.get_active() == true {
+            tool_state_clone.replace(Some(Tool::Pencil));
+            eraser_button_clone.set_active(false);
+        }
+    });
 
     tool_box.pack_start(&pencil_button, false, false, 0);
     tool_box.pack_start(&eraser_button, false, false, 0);
